b4a0ebce00fea86e779787399e14b5415134bf45
Bastian Hunecke <bastianhunecke@gmail.com>
2026-01-10 21:52:23 +0100

    add Robotiq ft sensor example

---
+++ src/example/robotiq_ft_example.c
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0
+#include <robif2b/functions/robotiq_ft_sensor.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    float fx, fy, fz, tx, ty, tz;
+    float wrench[6];
+    enum robif2b_robotiq_ft_state state;
+    bool success, new_data;
+
+    struct robif2b_robotiq_ft_nbx sensor = {
+        .conf = { .device = "/dev/ttyUSB0", .baudrate = 19200 },
+        .force_x = &fx, .force_y = &fy, .force_z = &fz,
+        .torque_x = &tx, .torque_y = &ty, .torque_z = &tz,
+        .wrench = wrench,
+        .state = &state,
+        .success = &success,
+        .new_data = &new_data,
+    };
+
+    robif2b_robotiq_ft_configure(&sensor);
+    if (!success) {
+        printf("Failed to configure sensor\n");
+        return 1;
+    }
+    printf("Sensor: %s, FW: %s\n", sensor.serial_number, sensor.firmware_version);
+
+    robif2b_robotiq_ft_start(&sensor);
+
+    robif2b_robotiq_ft_zero(&sensor);
+
+    for (int i = 0; i < 1000; i++) {
+        robif2b_robotiq_ft_update(&sensor);
+        if (new_data) {
+            printf("F: [%6.2f, %6.2f, %6.2f] N  T: [%6.3f, %6.3f, %6.3f] Nm\n",
+                   fx, fy, fz, tx, ty, tz);
+        }
+        usleep(4000);
+    }
+
+    robif2b_robotiq_ft_stop(&sensor);
+    robif2b_robotiq_ft_shutdown(&sensor);
+
+    return 0;
+}
